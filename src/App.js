1f740a81335ba347a7cc37e8666d8ccc308fdd47
Ankit Gandhi <ankitgandhi@Ankits-MacBook-Pro.local>
2019-12-26 00:34:10 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+  render() {
+    return <h1>Hello World from React boilerplate</h1>;
+  }
+}
+
+export default App;
